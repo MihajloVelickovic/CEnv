90e939527fd4bba73910ace8ff20001bcc28d3e1
Mihajlo Velickovic <permutator@elfak.rs>
2024-06-25 23:15:19 +0200

    Added support for referencing previously declared variables later in the file
    Signed-off-by: MihajloVelickovic <mvelickovic03@gmail.com>

--- src/cenv.c
+++ src/cenv.c
@@ -7,7 +7,7 @@ int* count_env = NULL;
 int count_calls = 0;
 char*** envvars = NULL;
 
-int count_rows(FILE* file, int* buffer_size_arg){
+static int count_rows(FILE* file, int* buffer_size_arg){
 	
 	int rows = 1;
 	const int buffer_size = buffer_size_arg ? *buffer_size_arg : 1024;
@@ -30,6 +30,45 @@ int count_rows(FILE* file, int* buffer_size_arg){
 	free(buffer);
 	return rows;
 	
+}
+
+static int check_references(const char* value, char** returner){
+	char** retval = (char**) malloc(sizeof(char*));
+	*returner = NULL;
+	int count = 0;
+	retval[count] = (char*) malloc(sizeof(char) * strlen(value) + 1);
+	char* dollar_open_brace = strstr(retval[count], "${");
+	char* closed_brace = strstr(retval[count], "}");
+	int diff, broken = 0;
+	strcpy(retval[count], value);
+	while(dollar_open_brace && closed_brace && 
+		 (diff = closed_brace - dollar_open_brace) > 0){
+			if(diff - 2 <= 0)
+				return -1;
+			char ref[diff-1];
+			strncpy(ref, dollar_open_brace + 2, diff-2);
+			ref[diff-2] = '\0';
+			char* var = getenv(ref);
+			if(!var){		
+				return -2;
+			}
+			int first_part = dollar_open_brace - retval[count];
+			int second_part = retval[count] + strlen(retval[count]) - closed_brace;
+			int var_part = strlen(var);
+			retval = (char**) realloc(retval, sizeof(retval) + sizeof(char*));
+			retval[++count] = (char*) malloc(sizeof(char) * (first_part + second_part + var_part));
+			strncpy(retval[count], retval[count-1], dollar_open_brace - retval[count-1]);
+			strcat(retval[count], var);
+			strcat(retval[count], closed_brace + 1);
+			dollar_open_brace = strstr(retval[count] + first_part + var_part, "${");
+			closed_brace = strstr(retval[count] + first_part + var_part, "}");
+	}
+
+	*returner = strdup(retval[count]);
+	for(int i=0; i<=count; ++i)
+		free(retval[i]);
+	free(retval);
+	return 0;
 }
 
 int load_env_vars(const char* fullpath){
@@ -69,6 +108,14 @@ int load_env_vars(const char* fullpath){
 		}
 
 		char* value = strtok(NULL, "\n");
+		char* ref;
+		int ref_checker = check_references(value, &ref);
+		if(ref_checker < 0){
+			fprintf(stderr, "Error populating %s\n", name);
+			return -1;
+		}
+		if(ref)
+			value = ref;
 		if(setenv(name, value, 0) < 0)
 			fprintf(stderr, "Error setting variable %s\n", name);
 		
@@ -77,8 +124,8 @@ int load_env_vars(const char* fullpath){
 			strcpy(envvars[count_calls][successes], name);
 			++successes;
 		}
+		free(ref);
 	}
-
 	fclose(envfile);
 	if(!count_env)
 		count_env = (int*) malloc(sizeof(int));
@@ -101,4 +148,6 @@ void unload_env_vars(){
 	free(count_env);
 	free(envvars);
 	envvars = NULL;
+	count_env = NULL;
+	count_calls = 0;
 }
